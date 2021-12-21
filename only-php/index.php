5d57d1363a005f2a650eb1a0fa2a30b179f6923d
abdi-El <abdila1099@gmail.com>
2021-12-21 13:22:39 +0100

    first commit

---
+++ only-php/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '/scripts/database.php'
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+    <title>Only PHP</title>
+</head>
+<body>
+    <header>
+        <img src="../img/logo.png" alt="logo">
+    </header>
+    <main></main>
+</body>
+
+</html>
